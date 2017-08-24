cbca1cde66c33c7257bd0074693ad856cf4b6123
Marcel Hernandez <e6990620@gmail.com>
2017-08-25 01:15:57 +0200

    introduced testing environment

--- src/Defaults/ApiKeyProvider.php
+++ src/Defaults/ApiKeyProvider.php
@@ -5,6 +5,12 @@ namespace UMA\Slim\Psr7Hmac\Defaults;
 use Psr\Http\Message\ServerRequestInterface;
 use UMA\Slim\Psr7Hmac\KeyProviderInterface;
 
+/**
+ * Most common implementation of the KeyProviderInterface.
+ *
+ * This KeyProvider retrieves the Api Key from one of the headers
+ * of the HTTP request.
+ */
 class ApiKeyProvider implements KeyProviderInterface
 {
     const DEFAULT_API_KEY_HEADER = 'Api-Key';
@@ -14,6 +20,9 @@ class ApiKeyProvider implements KeyProviderInterface
      */
     private $apiKeyHeader;
 
+    /**
+     * @param string $apiKeyHeader
+     */
     public function __construct($apiKeyHeader = self::DEFAULT_API_KEY_HEADER)
     {
         $this->apiKeyHeader = $apiKeyHeader;
@@ -26,7 +35,7 @@ class ApiKeyProvider implements KeyProviderInterface
     {
         $apiKey = $request->getHeaderLine($this->apiKeyHeader);
 
-        return '' === $apiKey ?
-            null : $apiKey;
+        return '' !== $apiKey ?
+            $apiKey : null;
     }
 }
---
+++ tests/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use UMA\Slim\Psr7Hmac\Defaults\HardcodedSecretProvider;
+use UMA\Slim\Psr7Hmac\Defaults\NullKeyProvider;
+use UMA\Slim\Psr7Hmac\Defaults\UnauthenticatedHandler;
+use UMA\Slim\Psr7Hmac\Psr7HmacAuthentication;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$cnt = new Container(['settings' => ['displayErrorDetails' => true]]);
+
+$cnt['hmac_secret'] = 'a_secret';
+
+$cnt[Psr7HmacAuthentication::class] = function ($cnt) {
+    return new Psr7HmacAuthentication(
+        new NullKeyProvider,
+        new HardcodedSecretProvider($cnt['hmac_secret']),
+        new UnauthenticatedHandler
+    );
+};
+
+$app = new \Slim\App($cnt);
+
+$app->get('/foo', function (Request $request, Response $response) {
+    $response->getBody()->write("Successful authentication!\n");
+
+    return $response->withStatus(202);
+})->add(Psr7HmacAuthentication::class);
+
+$app->run();
