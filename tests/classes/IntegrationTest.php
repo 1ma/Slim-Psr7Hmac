e9c6b14ee59f6bba74597b20da010be96519a5ac
Marcel Hernandez <e6990620@gmail.com>
2017-08-26 21:39:21 +0200

    test adding extra unsigned headers and tampering a request

--- tests/classes/IntegrationTest.php
+++ tests/classes/IntegrationTest.php
@@ -19,7 +19,7 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $this->http = new Client([RequestOptions::HTTP_ERRORS => false]);
     }
 
-    public function testOk()
+    public function testSignedRequest()
     {
         $signedRequest = (new Signer('a_secret'))
             ->sign(new Request('GET', 'http://slim-psr7hmac.test/foo'));
@@ -29,11 +29,35 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         self::assertSame(202, $response->getStatusCode());
     }
 
-    public function testKo()
+    public function testAddingExtraHeadersToSignedRequest()
+    {
+        $signedRequest = (new Signer('a_secret'))
+            ->sign(new Request('GET', 'http://slim-psr7hmac.test/foo'));
+
+        $response = $this->http->send(
+            $signedRequest->withHeader('Accept', 'text/html')
+        );
+
+        self::assertSame(202, $response->getStatusCode());
+    }
+
+    public function testUnsignedRequest()
     {
         $request = new Request('GET', 'http://slim-psr7hmac.test/foo');
 
         $response = $this->http->send($request);
+
+        self::assertSame(401, $response->getStatusCode());
+    }
+
+    public function testTamperedHeader()
+    {
+        $signedRequest = (new Signer('a_secret'))
+            ->sign(new Request('GET', 'http://slim-psr7hmac.test/foo', ['Accept' => 'text/html']));
+
+        $response = $this->http->send(
+            $signedRequest->withHeader('Accept', 'application/json')
+        );
 
         self::assertSame(401, $response->getStatusCode());
     }
